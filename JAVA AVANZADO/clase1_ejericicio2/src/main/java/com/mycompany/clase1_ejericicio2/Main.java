9892eb704c2f1874894e60f9044799a541f8bf85
Jaime Dodero <131529197+jaimed411@users.noreply.github.com>
2024-01-01 19:41:02 +0100

    Add files via upload

--- JAVA AVANZADO/clase1_ejericicio2/src/main/java/com/mycompany/clase1_ejericicio2/Main.java
+++ JAVA AVANZADO/clase1_ejericicio2/src/main/java/com/mycompany/clase1_ejericicio2/Main.java
@@ -1,3 +1,13 @@
+/*Ejercicio Nº 2 - Manejo de Inventarios en un Comercio - Individual
+Imagina que estás desarrollando un sistema de gestión de inventarios para un comercio minorista. 
+Los usuarios pueden agregar productos al inventario proporcionando información como el nombre del 
+producto, el precio y la cantidad disponible. Tu tarea es implementar una función que maneje la 
+gestión de inventarios, validando los datos ingresados y manejando excepciones en caso de errores
+o datos inválidos.
+-Trata de ser lo más descriptivo posible en cada mensaje de excepción (en caso de que ésta sea 
+opersonalizada) o de utilizar los tipos correctos de excepción en cada caso. 
+*/
+
 package com.mycompany.clase1_ejericicio2;
 
 public class Main {
